c601ef6c25c04ed8c74dfad1205bbbcb69ee038a
ukBaz <barry_byford@yahoo.co.uk>
2016-02-14 21:56:56 +0000

    travis install from script experiment

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,13 +2,12 @@
 case "${TRAVIS_PYTHON_VERSION}" in
     2.7)
         # Sort out loading of python modules
-        echo $PYTHONPATH
-        echo $PATH
-        python -c "import site; print(site.getsitepackages())"
+        sudo apt-get install -qq python-gi
+        sudo apt-get install -qq python-dbus
         ;;
     3.3)
-        echo $PYTHONPATH
-        echo $PATH
-        python3 -c "import site; print(site.getsitepackages())"
+        # load python 3 modules
+        sudo apt-get install -qq python3-gi
+        sudo apt-get install -qq python3-dbus
         ;;
 esac
