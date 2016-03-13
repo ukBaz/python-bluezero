b8a8197833b0b1ac4deb2e617fd3496b1b6c7e02
ukBaz <barry_byford@yahoo.co.uk>
2016-03-13 22:38:49 +0000

    pep8 fixes

--- examples/light_switch.py
+++ examples/light_switch.py
@@ -9,8 +9,8 @@ from gpiozero import Button
 import sys
 import os
 # sys.path.insert(0, '/home/pi/python/python-bluezero')
-sys.path.insert(0, os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
-# print('Directory /home/pi/python/python-bluezero', os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+sys.path.insert(0,
+                os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
 from bluezero import peripheral
 from signal import pause
 
@@ -18,6 +18,7 @@ from signal import pause
 led = LED(24)
 button = Button(25)
 LED_on = False
+
 
 def ble_switch_callback():
     print('Call-back', led.is_lit, switch_characteristic.ReadValue())
@@ -50,13 +51,17 @@ button.when_pressed = button_callback
 
 # Bluetooth
 # Service
-light_service = peripheral.Service('12341000-1234-1234-1234-123456789abc', True)
+light_service = peripheral.Service(
+    '12341000-1234-1234-1234-123456789abc',
+    True)
+
 print('**light service', light_service)
 
 # switch
-switch_characteristic = peripheral.Characteristic('12341001-1234-1234-1234-123456789abc',
-                                                  ['read', 'write'],
-                                                  light_service)
+switch_characteristic = peripheral.Characteristic(
+    '12341001-1234-1234-1234-123456789abc',
+    ['read', 'write'],
+    light_service)
 switch_characteristic.add_notify_event(ble_switch_callback)
 switch_characteristic.StartNotify()
 switch_descriptor = peripheral.UserDescriptor('Switch', switch_characteristic)
@@ -65,9 +70,10 @@ print('**Path: ', light_service.get_characteristics())
 light_service.add_characteristic(switch_characteristic)
 
 # state
-state_characteristic = peripheral.Characteristic('12341002-1234-1234-1234-123456789abc',
-                                                 ['notify'],
-                                                 light_service)
+state_characteristic = peripheral.Characteristic(
+    '12341002-1234-1234-1234-123456789abc',
+    ['notify'],
+    light_service)
 state_descriptor = peripheral.UserDescriptor('State', state_characteristic)
 state_characteristic.add_descriptor(switch_descriptor)
 light_service.add_characteristic(state_characteristic)
