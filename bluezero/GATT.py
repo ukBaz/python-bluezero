6c8a64b3669ceb5a0f45e05b919a67e0ed1fdb00
Barry Byford <barry_byford@yahoo.co.uk>
2016-07-31 20:56:29 +0100

    GATT Level 3 with notifications working (#48)

---
+++ bluezero/GATT.py
@@ -0,0 +1,163 @@
+import dbus
+import dbus.mainloop.glib
+from gi.repository import GLib
+
+from bluezero import constants
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+mainloop = GLib.MainLoop()
+bus = dbus.SystemBus(mainloop)
+
+
+def generic_error_cb(error):
+    print('D-Bus call failed: ' + str(error))
+    mainloop.quit()
+
+
+class Service:
+    def __init__(self, service_path):
+        self.service_path = service_path
+        self.service_methods = dbus.Interface(
+            bus.get_object(
+                constants.BLUEZ_SERVICE_NAME,
+                self.service_path),
+            constants.ADAPTER_INTERFACE)
+        self.service_props = dbus.Interface(
+            bus.get_object(
+                constants.BLUEZ_SERVICE_NAME,
+                self.service_path),
+            dbus.PROPERTIES_IFACE)
+
+    def UUID(self):
+        """
+        Returns value of Service UUID for this path
+        :return: string for example '00001800-0000-1000-8000-00805f9b34fb'
+        """
+        return self.service_props.Get(
+            constants.GATT_SERVICE_IFACE, 'UUID')
+
+    def device(self):
+        """
+        Returns the DBus object that this service belongs to
+        :return: DBus object of device
+        """
+        return self.service_props.Get(
+            constants.GATT_SERVICE_IFACE, 'Device')
+
+    def Primary(self):
+        """
+        Returns a boolean saying if this a primary service
+        :return: boolean
+        """
+        return self.service_props.Get(
+            constants.GATT_SERVICE_IFACE, 'Primary')
+
+
+class Characteristic:
+    def __init__(self, characteristic_path):
+        self.characteristic_path = characteristic_path
+        self.characteristic_methods = dbus.Interface(
+            bus.get_object(
+                constants.BLUEZ_SERVICE_NAME,
+                self.characteristic_path),
+            constants.GATT_CHRC_IFACE)
+        self.characteristic_props = dbus.Interface(
+            bus.get_object(
+                constants.BLUEZ_SERVICE_NAME,
+                self.characteristic_path),
+            dbus.PROPERTIES_IFACE)
+
+    def UUID(self):
+        """
+        Returns value of Characteristic UUID for this path
+        :return: string example '00002a00-0000-1000-8000-00805f9b34fb'
+        """
+        return self.characteristic_props.Get(
+            constants.GATT_CHRC_IFACE, 'UUID')
+
+    def service(self):
+        """
+        Returns the DBus object for the service that this characteristic
+        belongs to
+        :return: DBus object of device
+        """
+        return self.characteristic_props.Get(
+            constants.GATT_CHRC_IFACE, 'Service')
+
+    def notifying(self):
+        """
+        Returns a boolean of if this characteristic has notifications enabled
+        :return: Boolean
+        """
+        return self.characteristic_props.Get(
+            constants.GATT_CHRC_IFACE, 'Notifying')
+
+    def flags(self):
+        """
+        Returns a list of how this characteristic value can be used
+        :return: list example ['read', 'write', 'notify']
+        """
+        return self.characteristic_props.Get(
+            constants.GATT_CHRC_IFACE, 'Flags')
+
+    def read_raw_value(self, options=''):
+        """
+        Return the characteristic value if allowed
+        :param options:
+        :return:
+
+        Possible Errors:    org.bluez.Error.Failed
+                            org.bluez.Error.InProgress
+                            org.bluez.Error.NotPermitted
+                            org.bluez.Error.InvalidValueLength
+                            org.bluez.Error.NotAuthorized
+                            org.bluez.Error.NotSupported
+        """
+        return self.characteristic_methods.ReadValue(dbus.Array(options))
+
+    def write_value(self, value, options):
+        """
+        Write a new value to the characteristic
+        :param value:
+        :param options:
+        :return:
+        """
+        self.characteristic_methods.WriteValue(value, dbus.Array(options))
+
+    def start_notify(self):
+        self.characteristic_methods.StartNotify(
+            reply_handler=self.start_notify_cb,
+            error_handler=generic_error_cb,
+            dbus_interface=constants.GATT_CHRC_IFACE)
+
+    def stop_notify(self):
+        self.characteristic_methods.StopNotify(
+            reply_handler=self.stop_notify_cb,
+            error_handler=generic_error_cb,
+            dbus_interface=constants.GATT_CHRC_IFACE)
+
+    def add_characteristic_cb(self, callback=None):
+        if callback is None:
+            callback = self.props_changed_cb
+
+        self.characteristic_props.connect_to_signal('PropertiesChanged',
+                                                    callback)
+
+    def props_changed_cb(self, iface, changed_props, invalidated_props):
+        if iface != constants.GATT_CHRC_IFACE:
+            return
+
+        if not len(changed_props):
+            return
+
+        value = changed_props.get('Value', None)
+        if not value:
+            return
+
+        print('Properties changed: ', value)
+
+    def start_notify_cb(self):
+        print('Notifications enabled')
+
+    def stop_notify_cb(self):
+        print('Notifications disabled')
