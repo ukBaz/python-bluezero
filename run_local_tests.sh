4608fd791175f39e517601c4f0b562622382827e
ukBaz <barry_byford@yahoo.co.uk>
2016-08-05 09:14:39 +0100

    Enable device unit tests

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -3,6 +3,8 @@ python -m unittest -v tests.test_url_to_hex
 python3 -m unittest -v tests.test_url_to_hex
 python -m unittest -v tests.test_adapter
 python3 -m unittest -v tests.test_adapter
+python -m unittest -v tests.test_device
+python3 -m unittest -v tests.test_device
 pep8 bluezero
 pep8 --ignore=E402 examples
 pep8 tests
