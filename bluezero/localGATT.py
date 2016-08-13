a9ef27ad2f47975e448906d0f5425b84d0875bb4
ukBaz <barry_byford@yahoo.co.uk>
2016-08-13 23:39:46 +0100

    Characteristic and descriptor classes

--- bluezero/localGATT.py
+++ bluezero/localGATT.py
@@ -43,10 +43,15 @@ class Application(dbus.service.Object):
     This is the parent class for a python Bluez application.
 
     :Example:
-
+    >>> from bluezero import GATT
+    >>> from bluezero import localGATT
+    >>> from bluezero import tools
     >>> app = localGATT.Application()
-    >>> app.add_service(Service)
-    >>> app.start()
+    >>> srv = localGATT.Service(1, '180F', True)
+    >>> app.add_managed_object(srv)
+    >>> srv_mng = GATT.GattManager('/org/bluez/hci0')
+    >>> srv_mng.register_application(app.get_path(), {})
+    >>> tools.start_mainloop()
     """
     def __init__(self, device_id=None):
         """Default initialiser.
@@ -75,7 +80,8 @@ class Application(dbus.service.Object):
         response = {}
 
         for object in self.managed_objs:
-            response[object.get_path()] = object.get_properties()
+            iface = object.interface
+            response[object.get_path()] = {iface: object.GetAll(iface)}
 
         return response
 
@@ -98,7 +104,7 @@ class Service(dbus.service.Object):
 
     :Example:
 
-    >>> your_service = localGATT.Service(UUID, primary)
+    >>> your_service = localGATT.Service(service_id, UUID, primary)
 
     """
 
@@ -115,35 +121,22 @@ class Service(dbus.service.Object):
         :param primary: whether or not the service is a primary service
         """
         # Setup D-Bus object paths and register service
-        self.path = self.PATH_BASE + str(service_id)
+        self.path = self.PATH_BASE + str('{0:04d}'.format(service_id))
         self.bus = dbus.SystemBus()
+        self.interface = constants.GATT_SERVICE_IFACE
         dbus.service.Object.__init__(self, self.bus, self.path)
-        self.uuid = uuid
-        self.primary = primary
+        self.props = {constants.GATT_SERVICE_IFACE: {'UUID': uuid,
+                                                     'Primary': primary}
+                      }
 
     def get_path(self):
         """Return the DBus object path"""
         return dbus.ObjectPath(self.path)
 
-    def get_properties(self):
-        """Return a dictionary of the service properties.
-
-        The dictionary has the following keys:
-
-        - UUID: the service UUID
-        - Primary: whether the service is the primary service
-        """
-        return {
-            constants.GATT_SERVICE_IFACE: {
-                'UUID': self.uuid,
-                'Primary': self.primary
-            }
-        }
-
     @dbus.service.method(constants.DBUS_PROP_IFACE,
                          in_signature='s',
                          out_signature='a{sv}')
-    def GetAll(self, interface):
+    def GetAll(self, interface_name):
         """Return the service properties.
 
         This method is registered with the D-Bus at
@@ -154,7 +147,361 @@ class Service(dbus.service.Object):
         The interface must be ``org.bluez.GattService1`` otherwise an
         exception is raised.
         """
-        if interface != constants.GATT_SERVICE_IFACE:
+        if interface_name != constants.GATT_SERVICE_IFACE:
             raise InvalidArgsException()
 
-        return self.get_properties()
+        try:
+            return self.props[interface_name]
+        except KeyError:
+            raise dbus.exceptions.DBusException(
+                'no such interface ' + interface_name,
+                name=interface_name + '.UnknownInterface')
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ss', out_signature='v')
+    def Get(self, interface_name, property_name):
+        """DBus API for getting a property value"""
+
+        if interface_name != constants.GATT_SERVICE_IFACE:
+            raise InvalidArgsException()
+
+        try:
+            return self.GetAll(interface_name)[property_name]
+        except KeyError:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=interface_name + '.UnknownProperty')
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ssv', out_signature='')
+    def Set(self, interface_name, property_name, value, *args, **kwargs):
+        """Standard D-Bus API for setting a property value"""
+
+        try:
+            iface_props = self.props[interface_name]
+        except KeyError:
+            raise dbus.exceptions.DBusException(
+                'no such interface ' + interface_name,
+                name=self.interface + '.UnknownInterface')
+
+        if property_name not in iface_props:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=self.interface + '.UnknownProperty')
+
+        iface_props[property_name] = value
+
+        self.PropertiesChanged(interface_name,
+                               dbus.Dictionary({property_name: value},
+                                               signature='sv'),
+                               dbus.Array([], signature='s'))
+
+    @dbus.service.signal(constants.DBUS_PROP_IFACE,
+                         signature='sa{sv}as')
+    def PropertiesChanged(self, interface, changed, invalidated):
+        """Emit a Properties Changed notification signal.
+
+        This signal is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``.
+        """
+        pass
+
+
+class Characteristic(dbus.service.Object):
+    """Bluez Characteristic Class.
+
+    This class represents a BLE Characteristic.
+
+    :Example:
+
+    >>> your_characteristic = localGATT.Characteristic(characteristic_id,
+    >>>                                                UUID,
+    >>>                                                service_obj,
+    >>>                                                value,
+    >>>                                                notifying,
+    >>>                                                flags)
+    """
+    def __init__(self, characteristic_id,
+                 uuid,
+                 service_obj,
+                 value,
+                 notifying,
+                 flags):
+        """Default initialiser.
+
+        1. Registers the characteristc on the D-Bus.
+        2. Sets up the service UUID and primary flags.
+
+        :param service_id:
+        :param uuid: service BLE UUID
+        :param service_obj: the service that this characteristic is part of
+        :param value: the initial value of this characteristic
+        :param notifying: boolean representing the state of notification
+        :param flags:
+        """
+        # Setup D-Bus object paths and register service
+        PATH_BASE = service_obj.get_path() + '/char'
+        self.path = PATH_BASE + str('{0:04d}'.format(characteristic_id))
+        self.bus = dbus.SystemBus()
+        dbus.service.Object.__init__(self, self.bus, self.path)
+        self.props = {'UUID': uuid,
+                      'Service': service_obj.get_path(),
+                      'Value': value,
+                      'Notify': notifiying,
+                      'Flags': flags}
+
+        for prop in self.props.keys():
+            self.Set(constants.GATT_DESC_IFACE, prop, self.props[prop])
+
+    def get_path(self):
+        """Return the DBus object path"""
+        return dbus.ObjectPath(self.path)
+
+    @dbus.service.method(constants.DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface_name):
+        """Return the service properties.
+
+        This method is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``
+
+        :param interface: interface to get the properties of.
+
+        The interface must be ``org.bluez.GattCharacteristic1`` otherwise an
+        exception is raised.
+        """
+        if interface_name != constants.GATT_CHRC_IFACE:
+            raise InvalidArgsException()
+
+        return {
+            constants.GATT_CHRC_IFACE: self.props
+        }
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ss', out_signature='v')
+    def Get(self, interface_name, property_name):
+        """DBus API for getting a property value.
+
+        This method is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``
+
+        :param interface_name: interface to get the properties of.
+        :param property_name: request this property
+        """
+
+        if interface_name != constants.GATT_CHRC_IFACE:
+            raise InvalidArgsException()
+
+        try:
+            return self.GetAll(interface_name)[property_name]
+        except KeyError:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=interface_name + '.UnknownProperty')
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ssv', out_signature='')
+    def Set(self, interface_name, property_name, value, *args, **kwargs):
+        """Standard D-Bus API for setting a property value"""
+
+        if property_name not in self.props:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=constants.GATT_CHRC_IFACE + '.UnknownProperty')
+
+        self.props[property_name] = value
+
+        if bool(self.props['Notifying']) is True:
+            self.EmitSignal(dbus.PROPERTIES_IFACE,
+                            'PropertiesChanged',
+                            'sa{sv}as',
+                            [interface_name,
+                             dbus.Dictionary({property_name: value},
+                                             signature='sv'),
+                             dbus.Array([], signature='s')
+                             ])
+
+    @dbus.service.method(constants.GATT_CHRC_IFACE,
+                         in_signature='', out_signature='v')
+    def ReadValue(self):
+        """
+        DBus method for getting the characteristic value
+        :return: value
+        """
+        return self.GetAll(constants.GATT_CHRC_IFACE)['Value']
+
+    @dbus.service.method(constants.GATT_CHRC_IFACE,
+                         in_signature='', out_signature='v')
+    def WriteValue(self, value):
+        """
+        DBus method for setting the characteristic value
+        :return: value
+        """
+        return self.Set(constants.GATT_CHRC_IFACE, 'Value', value)
+
+    @dbus.service.method(constants.GATT_CHRC_IFACE,
+                         in_signature='', out_signature='')
+    def StartNotify(self):
+        """
+        DBus method for enabling notifications of the characteristic value.
+        :return: value
+        """
+        if not self.props['Notifying'] is True:
+            print('Notifying already, nothing to do')
+            return
+
+        self.Set(constants.GATT_CHRC_IFACE,
+                 'Notifying',
+                 dbus.Boolean(True, variant_level=1))
+
+    @dbus.service.method(constants.GATT_CHRC_IFACE,
+                         in_signature='', out_signature='')
+    def StopNotify(self):
+        """
+        DBus method for disabling notifications of the characteristic value.
+        :return: value
+        """
+        if self.props['Notifying'] is False:
+            print('Not Notifying, nothing to do')
+            return
+
+        self.Set(constants.GATT_CHRC_IFACE,
+                 'Notifying',
+                 dbus.Boolean(False, variant_level=1))
+
+
+class Descriptor(dbus.service.Object):
+    """Bluez Descriptor Class.
+
+    This class represents a BLE Descriptor.
+
+    :Example:
+
+    >>> your_descriptor = localGATT.Descriptor(descriptor_id,
+    >>>                                        UUID,
+    >>>                                        your_char,
+    >>>                                        value,
+    >>>                                        flags)
+    """
+    def __init__(self,
+                 descriptor_id,
+                 uuid,
+                 characteristic_obj,
+                 value,
+                 flags):
+        """Default initialiser.
+
+        1. Registers the descriptor on the D-Bus.
+        2. Sets up the service UUID and primary flags.
+
+        :param descriptor_id: A unique identifier for this descriptor
+        :param uuid: descriptor BLE UUID
+        :param characteristic_obj: The characteristic that this descriptor
+                                   is related to
+        :param value: The initial value of the descriptor
+        :param flags: Flags specifying access permissions
+        """
+        # Setup D-Bus object paths and register service
+        PATH_BASE = characteristic_obj.get_path() + '/desc'
+        self.path = PATH_BASE + str('{0:04d}'.format(descriptor_id))
+        self.bus = dbus.SystemBus()
+        dbus.service.Object.__init__(self, self.bus, self.path)
+        self.props = {'UUID': uuid,
+                      'Characteristic': characteristic_obj.get_path(),
+                      'Value': value,
+                      'Flags': flags}
+        for prop in self.props.keys():
+            self.Set(constants.GATT_DESC_IFACE, prop, self.props[prop])
+
+    def get_path(self):
+        """Return the DBus object path"""
+        return dbus.ObjectPath(self.path)
+
+    @dbus.service.method(constants.DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface_name):
+        """Return the descriptor properties.
+
+        This method is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``
+
+        :param interface: interface to get the properties of.
+
+        The interface must be ``org.bluez.GattDescriptor1`` otherwise an
+        exception is raised.
+        """
+        if interface_name != constants.GATT_DESC_IFACE:
+            raise InvalidArgsException()
+
+        return {
+            constants.GATT_DESC_IFACE: self.props
+        }
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ss', out_signature='v')
+    def Get(self, interface_name, property_name):
+        """DBus API for getting a property value.
+
+        This method is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``
+
+        :param interface_name: interface to get the properties of.
+        :param property_name: request this property
+        """
+
+        if interface_name != constants.GATT_DESC_IFACE:
+            raise InvalidArgsException()
+
+        try:
+            return self.GetAll(interface_name)[property_name]
+        except KeyError:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=interface_name + '.UnknownProperty')
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                         in_signature='ssv', out_signature='')
+    def Set(self, interface_name, property_name, value, *args, **kwargs):
+        """Standard D-Bus API for setting a property value"""
+
+        if property_name not in self.props:
+            raise dbus.exceptions.DBusException(
+                'no such property ' + property_name,
+                name=constants.GATT_DESC_IFACE + '.UnknownProperty')
+
+        self.props[property_name] = value
+
+        self.PropertiesChanged(interface_name,
+                               dbus.Dictionary({property_name: value},
+                                               signature='sv'),
+                               dbus.Array([], signature='s'))
+
+    @dbus.service.signal(constants.DBUS_PROP_IFACE,
+                         signature='sa{sv}as')
+    def PropertiesChanged(self, interface, changed, invalidated):
+        """Emit a Properties Changed notification signal.
+
+        This signal is registered with the D-Bus at
+        ``org.freedesktop.DBus.Properties``.
+        """
+        pass
+
+    @dbus.service.method(constants.GATT_DESC_IFACE,
+                         in_signature='', out_signature='v')
+    def ReadValue(self):
+        """
+        DBus method for getting the characteristic value
+        :return: value
+        """
+        return self.GetAll(constants.GATT_DESC_IFACE)['Value']
+
+    @dbus.service.method(constants.GATT_DESC_IFACE,
+                         in_signature='v', out_signature='')
+    def WriteValue(self, value):
+        """
+        DBus method for setting the descriptor value
+        :return:
+        """
+        return self.Set(constants.GATT_DESC_IFACE, 'Value', value)
