4739f640ec6342268467c53de3d820bba62cca00
Barry Byford <barry_byford@yahoo.co.uk>
2016-09-02 09:20:15 +0100

    Examples use latest library updates (#68)
    The examples had become out of date. This update brings them up-to-date.
    Also moved more complex examples into different directories

--- examples/eddystone-url-beacon.py
+++ examples/eddystone-url-beacon.py
@@ -1,32 +1,3 @@
-import sys
-import os
+from bluezero import eddystone
 
-sys.path.insert(0,
-                os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
-from bluezero import peripheral
-from bluezero import tools
-
-# Bluetooth
-# Add beacon information
-ukBaz_beacon = peripheral.Service('FEAA', True, type='broadcast')
-service_data = tools.url_to_advert(
-    'https://github.com/ukBaz',
-    frame_type=0x10,
-    tx_power=0xFF)
-ukBaz_beacon.add_service_data(service_data)
-
-
-# Add application
-app = peripheral.Application()
-app.add_service(ukBaz_beacon)
-
-app.add_device_name('ukBazBeacon')
-
-# Start service and advertise
-try:
-    app.start()
-except KeyboardInterrupt:
-    print('KeyboardInterrupt')
-finally:
-    app.stop()
-    print('finally')
+eddystone.EddystoneURL('https://github.com/ukBaz')
---
+++ examples/level10/read_sensortag_CC2650.py
@@ -0,0 +1,123 @@
+"""
+This is a simple example of how to read the Ti Sensortag CC2650.
+
+The idea of this file is to show as simplified as possibly
+procedure. Hopefully this short file will give clarity and
+understanding on how to access the properties and methods
+of Bluez D-Bus interface for a Ti Sensortag.
+
+The information for the CC2650 comes from:
+http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User%27s_Guide
+
+This program makes some assumptions to keep the complexity down:
+1) The paths to characteristics are known
+2) The adapter is hci0
+3) A scan has happened and the sensortag is in the device list
+   (Probably true as you will have had to get the paths to characteristics)
+4) You are running Bluez 5.36 or higher (with the experimental flag)
+"""
+from time import sleep
+
+from bluezero import tools
+from bluezero import constants
+from bluezero import adapter
+from bluezero import device
+from bluezero.GATT import Characteristic
+
+
+def val_print(obj_prop, value):
+    """
+    Print out a string and a value
+    :param obj_prop: Descriptive string
+    :param value: Value to be printed
+    """
+    print('{0}: {1}'.format(obj_prop, value))
+
+
+def read_sensor(config_path, data_path):
+    """
+    Read the sensors on the cc2650.
+    Does this by turning on the sensor, reads and then turns off sensor.
+    :param config_path: The path to the configuration characteristic
+    :param data_path: The path to the data characteristic
+    :return:
+    """
+    # Set configuration value to 1 (enable sensor)
+    conf_val = config_path.read_raw_value()
+    val_print('Sensor enabled', conf_val)
+    config_path.write_value((1).to_bytes(1, byteorder='little'))
+    conf_val = config_path.read_raw_value()
+    val_print('Sensor enabled', conf_val)
+
+    # Read sensor data
+    opt_val = data_path.read_raw_value()
+    val_print('Raw Value', opt_val)
+
+    # set configuraton value to 0 (disable sensor)
+    config_path.read_raw_value()
+    config_path.write_value((0).to_bytes(1, byteorder='little'))
+    conf_val = config_path.read_raw_value()
+    val_print('Sensor enabled', conf_val)
+
+
+def client():
+    dongle = adapter.Adapter(adapter.list_adapters()[0])
+    if not dongle.powered():
+        dongle.powered(True)
+
+    dongle.nearby_discovery()
+    cc2650 = device.Device(tools.device_dbus_path(constants.DEVICE_INTERFACE,
+                                                  'SensorTag')[0])
+    # Connect to device
+    cc2650.connect()
+
+    while not cc2650.services_resolved():
+        sleep(0.5)
+
+    # constants
+    TMP_CONF_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA02-0451-4000-B000-000000000000')[0])
+    TMP_DATA_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA01-0451-4000-B000-000000000000')[0])
+    OPT_CONF_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA72-0451-4000-B000-000000000000')[0])
+    OPT_DATA_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA71-0451-4000-B000-000000000000')[0])
+    BAR_CONF_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA42-0451-4000-B000-000000000000')[0])
+    BAR_DATA_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA41-0451-4000-B000-000000000000')[0])
+    HUM_CONF_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA22-0451-4000-B000-000000000000')[0])
+    HUM_DATA_PATH = Characteristic(tools.uuid_dbus_path(
+        constants.GATT_CHRC_IFACE,
+        'F000AA21-0451-4000-B000-000000000000')[0])
+
+    # Read the connected status property
+    if cc2650.connected():
+        # IR Temperature Sensor
+        print('\nIR Temperature Sensor')
+        read_sensor(TMP_CONF_PATH, TMP_DATA_PATH)
+        # Optical Sensor
+        print('\nOptical Sensor')
+        read_sensor(OPT_CONF_PATH, OPT_DATA_PATH)
+        # Barometric Pressure Sensor
+        print('\nBarometric Pressure Sensor')
+        read_sensor(BAR_CONF_PATH, BAR_DATA_PATH)
+        # Humidity Sensor
+        print('\nHumidity Sensor')
+        read_sensor(HUM_CONF_PATH, HUM_DATA_PATH)
+
+    # Disconnect device
+    cc2650.disconnect()
+
+
+if __name__ == '__main__':
+    client()
