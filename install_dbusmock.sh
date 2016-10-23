ab0eb147d7b751ac4a63f4fac8057cbbbfe9864e
ukBaz <barry_byford@yahoo.co.uk>
2016-10-23 09:42:50 +0100

    Install dbusmock in tmp on travis

--- install_dbusmock.sh
+++ install_dbusmock.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-wget https://github.com/ukBaz/python-dbusmock/archive/bluez_gatt.tar.gz -O dbusmock.tar.gz
+wget https://github.com/ukBaz/python-dbusmock/archive/bluez_gatt.tar.gz -O /tmp/dbusmock.tar.gz
+cd /tmp
 tar -xvf dbusmock.tar.gz
