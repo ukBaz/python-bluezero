9fc2ccec1d5ad91cc6c658355504d47dd6acea9b
Mark Roberts <mark@camroberts.org.uk>
2016-11-03 22:26:01 +0000

    Tidying up and documenting the GATT service UUID test

--- tests/test_gatt.py
+++ tests/test_gatt.py
@@ -1,3 +1,5 @@
+"""Automated testing of GATT functionality using dbusmock."""
+
 import subprocess
 import sys
 import unittest
@@ -5,44 +7,47 @@ import unittest
 import dbus
 import dbusmock
 
-from bluezero.adapter import Adapter
-from bluezero.device import Device
 from bluezero import GATT
 
 
 class TestBluezeroService(dbusmock.DBusTestCase):
+    """Test class to exercise (remote) GATT Service Features."""
 
     @classmethod
     def setUpClass(klass):
+        """Initiate mock Bluez on a mock dbus."""
         klass.start_system_bus()
         klass.dbus_con = klass.get_dbus(True)
         (klass.p_mock, klass.obj_bluez) = klass.spawn_server_template(
             'bluez5', {}, stdout=subprocess.PIPE)
 
     def setUp(self):
+        """Initialise the class for the tests."""
         self.obj_bluez.Reset()
         self.dbusmock = dbus.Interface(self.obj_bluez, dbusmock.MOCK_IFACE)
         self.dbusmock_bluez = dbus.Interface(self.obj_bluez, 'org.bluez.Mock')
 
     def test_service_uuid(self):
+        """Test the service UUID."""
         adapter_name = 'hci0'
         address = '11:22:33:44:55:66'
         alias = 'Peripheral Device'
 
+        # Initialise an adapter and ensure that it's on the right path
         path = self.dbusmock_bluez.AddAdapter(adapter_name, 'my-computer')
         self.assertEqual(path, '/org/bluez/' + adapter_name)
-        dongle = Adapter('/org/bluez/hci0')
 
-        dev_path = self.dbusmock_bluez.AddDevice('hci0',
-                                                 '11:22:33:44:55:66',
-                                                 'Peripheral Device')
+        # Add a mock remote device
+        self.dbusmock_bluez.AddDevice(adapter_name, address, alias)
 
-        path = self.dbusmock_bluez.AddGATT('hci0',
-                                           '11:22:33:44:55:66',
-                                           'Peripheral Device')
+        # Initialise a mock remote GATT and ensure it's on the right path
+        path = self.dbusmock_bluez.AddGATT(adapter_name, address, alias)
         srvc_dbus_path = '/org/bluez/hci0/dev_11_22_33_44_55_66/service0001'
+
+        # Invoke the bluez GATT library to access the mock GATT service
         test_service = GATT.Service(srvc_dbus_path)
-        test_device = Device('/org/bluez/hci0/dev_11_22_33_44_55_66')
+
+        # Test for the UUID
         found_name = test_service.UUID
         self.assertEqual(found_name, '180F')
 
