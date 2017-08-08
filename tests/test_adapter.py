a7cdd336716d2c850ffcb80187df0c4f8c26dbdb
ukBaz <barry_byford@yahoo.co.uk>
2017-08-08 21:57:58 +0100

    Remove dbusmock and use unittest mock for Adapter

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,201 +1,140 @@
-import subprocess
-import sys
 import unittest
+from unittest.mock import MagicMock
+from unittest.mock import patch
+import tests.obj_data
+from bluezero import constants
 
-import dbus
-import dbusmock
-
-from bluezero.adapter import Adapter
+adapter_props = tests.obj_data.full_ubits
 
 
-class TestBluezeroAdapter(dbusmock.DBusTestCase):
+def mock_get(iface, prop):
+    return tests.obj_data.full_ubits['/org/bluez/hci0'][iface][prop]
 
-    @classmethod
-    def setUpClass(klass):
-        klass.start_system_bus()
-        klass.dbus_con = klass.get_dbus(True)
 
-        (klass.p_mock, klass.obj_bluez) = klass.spawn_server_template(
-            'bluez5', {}, stdout=subprocess.PIPE)
+def mock_set(iface, prop, value):
+    tests.obj_data.full_ubits['/org/bluez/hci0'][iface][prop] = value
+
+
+class TestBluezeroAdapter(unittest.TestCase):
 
     def setUp(self):
-        # bluetoothd
-        self.obj_bluez.Reset()
-        self.dbusmock = dbus.Interface(self.obj_bluez, dbusmock.MOCK_IFACE)
-        self.dbusmock_bluez = dbus.Interface(self.obj_bluez, 'org.bluez.Mock')
+        """
+        Patch the DBus module
+        :return:
+        """
+        self.dbus_mock = MagicMock()
+        self.mainloop_mock = MagicMock()
+        self.gobject_mock = MagicMock()
+
+        modules = {
+            'dbus': self.dbus_mock,
+            'dbus.mainloop.glib': self.mainloop_mock,
+            'gi.repository': self.gobject_mock,
+        }
+        self.dbus_mock.Interface.return_value.GetManagedObjects.return_value = tests.obj_data.full_ubits
+        self.dbus_mock.Interface.return_value.Get = mock_get
+        self.dbus_mock.Interface.return_value.Set = mock_set
+        self.module_patcher = patch.dict('sys.modules', modules)
+        self.module_patcher.start()
+        from bluezero import adapter
+        self.module_under_test = adapter
+        self.dbus_mock.Interface.return_value.Get
         self.adapter_device = 'hci0'
-        self.adapter_name = 'Linux SBC'
+        self.adapter_name = 'linaro-alip'
+        self.path = '/org/bluez/hci0'
+
+    def tearDown(self):
+        self.module_patcher.stop()
 
     def test_adapter_address(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
-        self.assertEqual(dongle.address, '00:01:02:03:04:05')
+        dongle = self.module_under_test.Adapter(self.path)
+        self.assertEqual(dongle.address, '00:00:00:00:5A:AD')
 
     def test_adapter_name(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         self.assertEqual(dongle.name, self.adapter_name)
 
     def test_adapter_alias(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         self.assertEqual(dongle.alias, self.adapter_name)
 
     def test_adapter_alias_write(self):
         dev_name = 'my-test-dev'
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.alias = dev_name
         self.assertEqual(dongle.alias, dev_name)
 
     def test_class(self):
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
-        self.assertEqual(dongle.bt_class, 268)
+        self.assertEqual(dongle.bt_class, 4980736)
 
     def test_adapter_power(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         self.assertEqual(dongle.powered, 1)
 
     def test_adapter_power_write(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.powered = 0
-        self.assertEqual(dongle.powered, 0)
+        self.assertEqual(dongle.powered, False)
 
     def test_adapter_discoverable(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
-        self.assertEqual(dongle.discoverable, 1)
+        self.assertEqual(dongle.discoverable, False)
 
     def test_adapter_discoverable_write(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
-        dongle.discoverable = 0
-        self.assertEqual(dongle.discoverable, 0)
+        dongle.discoverable = 1
+        self.assertEqual(dongle.discoverable, True)
 
     def test_adapter_pairable(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         self.assertEqual(dongle.pairable, 1)
 
     def test_adapter_pairable_write(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.pairable = 0
         self.assertEqual(dongle.pairable, 0)
 
     def test_adapter_pairabletimeout(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
-        self.assertEqual(dongle.pairabletimeout, 180)
+        self.assertEqual(dongle.pairabletimeout, 0)
 
     def test_adapter_pairabletimeout_write(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.pairabletimeout = 220
         self.assertEqual(dongle.pairabletimeout, 220)
 
     def test_adapter_discoverabletimeout(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         self.assertEqual(dongle.discoverabletimeout, 180)
 
     def test_adapter_discoverabletimeout_write(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.discoverabletimeout = 220
         self.assertEqual(dongle.discoverabletimeout, 220)
 
     def test_adapter_discovering(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
-        self.assertEqual(dongle.discovering, 1)
+        self.assertEqual(dongle.discovering, False)
 
     @unittest.skip('mock of discovery not implemented')
     def test_start_discovery(self):
-        # Add an adapter
-        path = self.dbusmock_bluez.AddAdapter(self.adapter_device,
-                                              self.adapter_name)
-
-        self.assertEqual(path, '/org/bluez/' + self.adapter_device)
-        dongle = Adapter(path)
+        dongle = self.module_under_test.Adapter(self.path)
         # test
         dongle.nearby_discovery()
         self.assertEqual(dongle.discovering, 1)
