9c880188173b79243df7d4de09bf66a5f031225c
Barry Byford <barry_byford@yahoo.co.uk>
2019-05-05 10:57:45 +0100

    Fix failing tests

--- examples/async_discover_and_pair.py
+++ examples/async_discover_and_pair.py
@@ -1,12 +1,12 @@
-from bluezero import adapter
-from bluezero import device
-from bluezero import async_tools
-from bluezero import constants
-from bluezero import microbit
 from gi.repository import GLib
+
+from bluezero import adapter
+from bluezero import async_tools
+from bluezero import microbit
 from time import sleep
 
 ubit_device = None
+
 
 def start_disco():
     print('Starting scan')
@@ -23,28 +23,32 @@ def device_found_handler(device_obj):
         while waiting:
             print('waiting...')
             sleep(1)
-            print('Status ubit', device_obj.connected, 
-                                 device_obj.services_resolved, 
-                                 device_obj.paired)
+            print('Status ubit',
+                  device_obj.connected,
+                  device_obj.services_resolved,
+                  device_obj.paired)
             waiting = not device_obj.paired
         if device_obj.paired:
             print('Dongle Addres', dongle.address, device_obj.address)
             ubit_device = microbit.Microbit(adapter_addr=dongle.address,
-                                 device_addr=device_obj.address,
-                                 accelerometer_service=False,
-                                 button_service=True,
-                                 led_service=False,
-                                 magnetometer_service=False,
-                                 pin_service=False,
-                                 temperature_service=False)
+                                            device_addr=device_obj.address,
+                                            accelerometer_service=False,
+                                            button_service=True,
+                                            led_service=False,
+                                            magnetometer_service=False,
+                                            pin_service=False,
+                                            temperature_service=False)
             print(dir(ubit_device))
-            # Need long sleep here to allow micro:bit to switch from pairing mode
-            # to normal mode. There should be a smarter way to do this I just can't
-            # think what it is right now
+            # Need long sleep here to allow micro:bit to switch from pairing
+            # mode to normal mode. There should be a smarter way to do this
+            # I just can't think what it is right now
             sleep(30)
-            print('call ubit', ubit_device.connected, ubit_device.ubit.services_resolved)
-            # Using native GLib timeout method because Bluezero one does not take parameters
-            # Should update the async_tools.add_timer method to accept them
+            print('call ubit',
+                  ubit_device.connected,
+                  ubit_device.ubit.services_resolved)
+            # Using native GLib timeout method because Bluezero one does
+            # not take parameters. Should update the async_tools.add_timer
+            # method to accept them
             GLib.timeout_add(500, connect_ubit, ubit_device)
     return True
 
@@ -57,7 +61,8 @@ def connect_ubit(new_device):
     new_device.subscribe_button_a(btn_a_action)
     print('Add button B')
     new_device.subscribe_button_b(btn_a_action)
-    False
+    return False
+
 
 def btn_a_action(*args, **kwargs):
     # Need to update micro:bit class so the user callback
@@ -66,10 +71,12 @@ def btn_a_action(*args, **kwargs):
     print('args = ', args)
     print('kwargs = ', kwargs)
 
+
 def btn_b_action(*args, **kwargs):
     print('button b')
     print('args = ', args)
     print('kwargs = ', kwargs)
+
 
 def exit_test():
     print('Times up!')
