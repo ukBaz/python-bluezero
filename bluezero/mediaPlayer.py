1293f47a812882fbd71aec81db73ac11e1a27eb8
FahirahD <fahirah.diarra@gmail.com>
2020-02-27 01:50:30 -0500

    created the MediaPlayer object to enable A2DP capability

---
+++ bluezero/mediaPlayer.py
@@ -0,0 +1,163 @@
+import dbus
+import logging
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+# python-bluezero imports
+from bluezero import constants
+from bluezero import dbus_tools
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+logger.addHandler(NullHandler())
+
+class MediaPlayer :
+    """Bluetooth MediaPlayer Class.
+
+    This class instantiates an object that is able to interact with the player of a
+    Bluetooth device and get audio from its source.
+    """
+    def __init__ (self,player_addr):
+        """Default initialiser.
+
+        Creates the interface to the remote Bluetooth device.
+
+        :param player_addr: Address of Bluetooth player to use.
+        """
+        self.player_object =  dbus_tools.get_dbus_obj(player_addr)
+        self.player_methods = dbus_tools.get_dbus_iface(constants.MEDIA_PLAYER_IFACE,self.player_object)
+        self.player_props = dbus_tools.get_dbus_iface(dbus.PROPERTIES_IFACE,self.player_object)
+
+    @property
+    def browsable(self):
+        """If present indicates the player can be browsed using MediaFolder interface."""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Browsable')
+
+    @property
+    def searchable(self):
+        """If present indicates the player can be searched using
+		MediaFolder interface.
+        """
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Searchable')
+
+    @property
+    def track(self):
+        """Return a dict of the track metadata."""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Track')
+    
+    @property
+    def device(self):
+        """Return Device object path"""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Device')
+    
+    @property
+    def playlist(self):
+        """Return the Playlist object path."""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Playlist')
+    
+    @property
+    def equalizer (self):
+         """Return the equalizer value."""
+         return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Equalizer')
+    
+    @equalizer.setter
+    def equalizer(self,value):
+        """Possible values: "off" or "on"."""
+        self.player_props.Set(
+            constants.MEDIA_PLAYER_IFACE, 'Equalizer', value)
+    
+    @property
+    def name(self):
+        """Return the player name"""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Name')
+
+    @property
+    def repeat(self):
+         """Return the repeat value"""
+         return self.payer_props.Get(constants.MEDIA_PLAYER_IFACE, 'Repeat')
+
+    @repeat.setter
+    def repeat(self,value):
+        """Possible values: "off", "singletrack", "alltracks" or
+		"group
+        """
+        self.player_props.Set(
+            constants.MEDIA_PLAYER_IFACE, 'Repeat', value)
+
+    @property  
+    def shuffle(self):
+        """Return the shuffle value"""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Shuffle')
+    
+    @shuffle.setter
+    def shuffle(self,value):
+        """"Possible values: "off", "alltracks" or "group" """
+        self.player_props.Set(constants.MEDIA_PLAYER_IFACE, 'Browsable',value)
+
+
+    @property
+    def status(self):
+        """Return the status of the player
+        Possible status: "playing", "stopped", "paused",
+		"forward-seek", "reverse-seek" or "error"
+        """
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Status')
+    
+    @property
+    def subtype(self):
+        """Return the player subtype"""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Subtype')
+
+    def type(self,type):
+        """Player type
+			Possible values:
+				"Audio"
+				"Video"
+				"Audio Broadcasting"
+				"Video Broadcasting"
+        """
+        self.player_props.Set(
+            constants.MEDIA_PLAYER_IFACE, 'Type', type)
+    
+    @property
+    def position(self):
+        """Return the playback position in milliseconds."""
+        return self.player_props.Get(constants.MEDIA_PLAYER_IFACE, 'Position')
+    
+    def next(self):
+        """Goes the next track and play it."""
+        self.player_methods.Next()
+
+    def play(self):
+        """Resume the playback."""
+        self.player_methods.Play()
+    
+    def pause(self):
+        """Pause the track."""
+        self.player_methods.Pause()
+
+    def stop(self):
+        """Stops the playback."""
+        self.player_methods.Stop()
+
+    def previous(self):
+        """Goes the previous track and play it"""
+        self.player_methods.Previous()
+
+    def fast_forward(self):
+        """Fast forward playback, this action is only stopped
+        when another method in this interface is called.
+        """
+        self.player_methods.FastForward()
+
+    def rewind(self):
+        """Rewind playback, this action is only stopped
+		when another method in this interface is called.
+        """
+        self.player_methods.Rewind()
