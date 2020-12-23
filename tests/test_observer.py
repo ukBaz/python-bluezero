8a9306d565e2a9a2d201308eef572e4b4d77ab76
ukBaz <barry_byford@yahoo.co.uk>
2020-12-23 14:29:11 +0000

    Refactor location of dbusmock templates

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -26,7 +26,7 @@ class TestBlueZ5(dbusmock.DBusTestCase):
     @classmethod
     def setUpClass(cls):
         here = Path(__file__).parent
-        template = str(here.joinpath('dbusmock_bluez_scan.py'))
+        template = str(here.joinpath('dbusmock_templates', 'bluez_scan.py'))
         cls.start_system_bus()
         cls.dbus_con = cls.get_dbus(True)
         (cls.p_mock, cls.obj_bluez) = cls.spawn_server_template(
