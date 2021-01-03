55dc0fbc2710a75c9e8f519428e6b10747bf6eb7
ukBaz <barry_byford@yahoo.co.uk>
2021-01-03 10:19:39 +0000

    Update classifier to Python 3 only for Bluezero

--- setup.py
+++ setup.py
@@ -67,8 +67,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3 :: Only'
     ],
 
     # What does your project relate to?
