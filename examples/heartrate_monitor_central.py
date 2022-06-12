40c02be75310ddfee9f9b6f2609f28927d5278f9
Chad Rockey <chadrockey@gmail.com>
2022-06-12 15:48:25 -0500

    pycodestyle lint fixes

--- examples/heartrate_monitor_central.py
+++ examples/heartrate_monitor_central.py
@@ -1,19 +1,24 @@
+"""Example of how to create a Central device/GATT Client"""
 from bluezero import adapter
 from bluezero import central
 
 HRM_SRV = '0000180D-0000-1000-8000-00805f9b34fb'
-HR_MSRMT_UUID =      '00002a37-0000-1000-8000-00805f9b34fb'
+HR_MSRMT_UUID = '00002a37-0000-1000-8000-00805f9b34fb'
 BODY_SNSR_LOC_UUID = '00002a38-0000-1000-8000-00805f9b34fb'
-HR_CTRL_PT_UUID =    '00002a39-0000-1000-8000-00805f9b34fb'
+HR_CTRL_PT_UUID = '00002a39-0000-1000-8000-00805f9b34fb'
 
-def scan_for_heartrate_monitors(adapter_address=None, hrm_address=None, timeout=5.0):
-    # If there are multiple adapters on your system, this will scan using all of them
-    # Unless an address is specified and filtered
+
+def scan_for_heartrate_monitors(
+        adapter_address=None,
+        hrm_address=None,
+        timeout=5.0):
+    # If there are multiple adapters on your system, this will scan using
+    # all dongles unless an adapter is specified through its MAC address
     for dongle in adapter.Adapter.available():
         # Filter dongles by adapter_address if specified
         if adapter_address and adapter_address.upper() != dongle.address():
             continue
-        
+
         # Actually listen to nearby advertisements for timeout seconds
         dongle.nearby_discovery(timeout=timeout)
 
@@ -27,11 +32,12 @@ def scan_for_heartrate_monitors(adapter_address=None, hrm_address=None, timeout=
             if HRM_SRV.lower() in dev.uuids:
                 yield dev
 
+
 def on_new_heart_rate_measurement(iface, changed_props, invalidated_props):
     value = changed_props.get('Value', None)
     if not value:
         return
-    
+
     # Check bits field
     if value[0] & 0x01:
         # 16 bit heartrate value
@@ -39,17 +45,21 @@ def on_new_heart_rate_measurement(iface, changed_props, invalidated_props):
     else:
         # 8 bit heartrate value
         hr = value[1]
-    
+
     print("Notified of a heartrate of %d" % (hr))
+
 
 def connect_and_run(dev=None, device_address=None):
     # Create Interface to Central
     if dev:
-        monitor = central.Central(adapter_addr=dev.adapter, device_addr=dev.address)
+        monitor = central.Central(
+            adapter_addr=dev.adapter,
+            device_addr=dev.address)
     else:
         monitor = central.Central(device_addr=device_address)
 
-    # Add characteristics that we're interested in before we connect so they automatically resolve
+    # Characteristics that we're interested must be added to the Central
+    # before we connect so they automatically resolve BLE properties
     # Heart Rate Measurement - notify
     measurement_char = monitor.add_characteristic(HRM_SRV, HR_MSRMT_UUID)
 
@@ -57,7 +67,7 @@ def connect_and_run(dev=None, device_address=None):
     location_char = monitor.add_characteristic(HRM_SRV, BODY_SNSR_LOC_UUID)
 
     # Heart Rate Control Point - write - not always supported
-    #control_point_char = monitor.add_characteristic(HRM_SRV, HR_CTRL_PT_UUID)
+    control_point_char = monitor.add_characteristic(HRM_SRV, HR_CTRL_PT_UUID)
 
     # Now Connect to the Device
     if dev:
@@ -72,19 +82,19 @@ def connect_and_run(dev=None, device_address=None):
         print("Didn't connect to device!")
         return
 
-
-    ### Example Usage!!!
+    # Example Usage!!!
 
     # Read the Body Sensor Location
-    print("The location of the heart rate sensor is: %d" % int(location_char.value[0]))
+    print("The location of the heart rate sensor is: %d" %
+          int(location_char.value[0]))
 
     # Write the Control Point Value
-    #control_point_char.value = 0
+    control_point_char.value = 1
 
     # Enable heart rate notifications
     measurement_char.start_notify()
     measurement_char.add_characteristic_cb(on_new_heart_rate_measurement)
-    
+
     try:
         # Startup in async mode to enable notify, etc
         monitor.run()
@@ -93,7 +103,6 @@ def connect_and_run(dev=None, device_address=None):
 
     measurement_char.stop_notify()
     monitor.disconnect()
-
 
 
 if __name__ == '__main__':
--- examples/heartrate_monitor_peripheral.py
+++ examples/heartrate_monitor_peripheral.py
@@ -9,12 +9,14 @@ from bluezero import adapter
 from bluezero import peripheral
 
 HRM_SRV = '0000180D-0000-1000-8000-00805f9b34fb'
-HR_MSRMT_UUID =      '00002a37-0000-1000-8000-00805f9b34fb'
+HR_MSRMT_UUID = '00002a37-0000-1000-8000-00805f9b34fb'
 BODY_SNSR_LOC_UUID = '00002a38-0000-1000-8000-00805f9b34fb'
-HR_CTRL_PT_UUID =    '00002a39-0000-1000-8000-00805f9b34fb'
+HR_CTRL_PT_UUID = '00002a39-0000-1000-8000-00805f9b34fb'
 
 
 heartrate = 60
+
+
 def read_heartrate():
     global heartrate
     """
@@ -33,12 +35,14 @@ def read_heartrate():
         heart_rate = 60
     return struct.pack('<BB', flags, heartrate)
 
+
 def read_sensor_location():
     # Static 1 is Chest location
     # Little endian, unsigned char
     sensor_location = 1
     print("Sending sensor location of %d" % (sensor_location))
     return struct.pack('<B', sensor_location)
+
 
 def update_value(characteristic):
     """
@@ -67,9 +71,11 @@ def notify_callback(notifying, characteristic):
     if notifying:
         async_tools.add_timer_seconds(1, update_value, characteristic)
 
+
 def write_control_point(value, options):
     control_point = struct.unpack('<B', bytes(value))[0]
     print("Recieved control point value of: %d" % (control_point))
+
 
 def main(adapter_address):
     """Creation of peripheral"""
@@ -78,35 +84,37 @@ def main(adapter_address):
 
     # Create peripheral
     hr_monitor = peripheral.Peripheral(adapter_address,
-                                        local_name='Heartrate Monitor',
-                                        appearance=0x0340)
+                                       local_name='Heartrate Monitor',
+                                       appearance=0x0340)
     # Add service
     hr_monitor.add_service(srv_id=1, uuid=HRM_SRV, primary=True)
 
     # Add characteristics
     hr_monitor.add_characteristic(srv_id=1, chr_id=1, uuid=HR_MSRMT_UUID,
-                                   value=[], notifying=False,
-                                   flags=['read', 'notify'], # May not exactly match standard, but including read for tutorial
-                                   read_callback=read_heartrate,
-                                   write_callback=None,
-                                   notify_callback=notify_callback
-                                   )
+                                  value=[], notifying=False,
+                                  # May not exactly match standard, but
+                                  # including read for tutorial
+                                  flags=['read', 'notify'],
+                                  read_callback=read_heartrate,
+                                  write_callback=None,
+                                  notify_callback=notify_callback
+                                  )
 
     hr_monitor.add_characteristic(srv_id=1, chr_id=2, uuid=BODY_SNSR_LOC_UUID,
-                                   value=[], notifying=False,
-                                   flags=['read'],
-                                   read_callback=read_sensor_location,
-                                   write_callback=None,
-                                   notify_callback=None
-                                   )
+                                  value=[], notifying=False,
+                                  flags=['read'],
+                                  read_callback=read_sensor_location,
+                                  write_callback=None,
+                                  notify_callback=None
+                                  )
 
     hr_monitor.add_characteristic(srv_id=1, chr_id=3, uuid=HR_CTRL_PT_UUID,
-                                   value=[], notifying=False,
-                                   flags=['write'],
-                                   read_callback=None,
-                                   write_callback=write_control_point,
-                                   notify_callback=None
-                                   )
+                                  value=[], notifying=False,
+                                  flags=['write'],
+                                  read_callback=None,
+                                  write_callback=write_control_point,
+                                  notify_callback=None
+                                  )
 
     # Publish peripheral and start event loop
     hr_monitor.publish()
